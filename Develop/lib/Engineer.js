382ce78d5114d81d7bdd5d6e62a375a7353bd17d
egibbs834 <edwardgibbons34@gmail.com>
2020-08-18 18:35:23 -0400

    finished employee test

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,3 +1,12 @@
+const Employee = require("./Employee");
+
+class Engineer {
+    constructor(name, id, email, github) {
+        super(name, id, email) 
+        this.email = email; 
+    }
+
+
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
 // In addition to `Employee`'s properties and methods, `Engineer` will also have:

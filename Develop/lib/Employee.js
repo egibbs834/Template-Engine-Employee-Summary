4282d3386fc42d9f94e1fdd5f58d12e28240b3c8
egibbs834 <edwardgibbons34@gmail.com>
2020-08-17 21:22:23 -0400

    comments added to guide workflow

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,12 @@
 // TODO: Write code to define and export the Employee class
+
+// he first class is an `Employee` parent class with the following properties and
+// methods:
+
+//   * name
+//   * id
+//   * email
+//   * getName()
+//   * getId()
+//   * getEmail()
+//   * getRole() // Returns 'Employee'
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,17 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// he first class is an `Employee` parent class with the following properties and
+// methods:
+
+//   * name
+//   * id
+//   * email
+//   * getName()
+//   * getId()
+//   * getEmail()
+//   * getRole() // Returns 'Employee'
+
+//   In addition to `Employee`'s properties and methods, `Manager` will also have:
+
+//   * officeNumber
+
+//   * getRole() // Overridden to return 'Manager'

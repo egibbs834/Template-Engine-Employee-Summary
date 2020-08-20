d5fa97b180eb04cd6b8299ece94dffcaf5c23894
egibbs834 <edwardgibbons34@gmail.com>
2020-08-19 20:13:16 -0400

    adding css stylings

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -21,17 +21,3 @@ class Employee {
 }
 
 module.exports = Employee; 
-
-
-// TODO: Write code to define and export the Employee class
-
-// he first class is an `Employee` parent class with the following properties and
-// methods:
-
-//   * name
-//   * id
-//   * email
-//   * getName()
-//   * getId()
-//   * getEmail()
-//   * getRole() // Returns 'Employee'
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -13,12 +13,3 @@ class Intern extends Employee {
 
 module.exports = Intern;
 
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-
-// In addition to `Employee`'s properties and methods, `Intern` will also have:
-
-//   * school 
-
-//   * getSchool()
-
-//   * getRole() // Overridden to return 'Intern'
